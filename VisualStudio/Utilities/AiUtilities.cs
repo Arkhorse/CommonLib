86f32d26d0bbd595bded64b0908eb6db5e1458aa
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 17:57:17 -0300

    Update AiUtilities.cs

--- VisualStudio/Utilities/AiUtilities.cs
+++ VisualStudio/Utilities/AiUtilities.cs
@@ -11,125 +11,110 @@
 namespace CommonLib.Utilities
 {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    public static class AiUtilities
+	public static class AiUtilities
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
-    {
+	{
+		/// <summary>
+		/// Checks if the current BaseAi is a ptarmigan
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsPtarmigan(BaseAi baseAi) => baseAi.Ptarmigan != null;
 		/// <summary>
 		/// Checks if the current BaseAi is a rabbit
 		/// </summary>
 		/// <param name="baseAi">The BaseAi instance</param>
 		/// <returns></returns>
-		public static bool IsRabbit(BaseAi baseAi) => baseAi.Rabbit;
-
+		public static bool IsRabbit(BaseAi baseAi) => baseAi.Rabbit != null;
 		/// <summary>
-		/// Checks if the current BaseAi is a bear
+		/// Gets all Deer
 		/// </summary>
 		/// <param name="baseAi">The BaseAi instance</param>
 		/// <returns></returns>
-		public static bool IsBear(BaseAi baseAi) => baseAi.Bear;
-
-        /// <summary>
-        /// Gets all wolves
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsBaseWolf(BaseAi baseAi) => baseAi.BaseWolf;
-
-        /// <summary>
-        /// Checks if the current BaseAi is a NormalWolf (anything not Timberwolf)
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsNormalWolf(BaseAi baseAi) => baseAi.NormalWolf;
-
-        /// <summary>
-        /// Checks if the current BaseAi is a timberwolf
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsTimberwolf(BaseAi baseAi) => baseAi.Timberwolf;
-
+		public static bool IsBaseDeer(BaseAi baseAi) => baseAi.BaseDeer != null;
 		/// <summary>
-		/// Checks if the current BaseAi is a Toxic Wolf
+		/// Checks if the current BaseAi is a doe
 		/// </summary>
-		/// <param name="baseAi"></param>
+		/// <param name="baseAi">The BaseAi instance</param>
 		/// <returns></returns>
-		public static bool IsToxicWolf(BaseAi baseAi) => baseAi.StarvingWolf;
-
+		public static bool IsDoe(BaseAi baseAi) => baseAi.Doe != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a Normal Stag
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsStag(BaseAi baseAi) => baseAi.Stag != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a White Stag
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsStagWhite(BaseAi baseAi) => baseAi.StagWhite != null;
 		/// <summary>
 		/// Checks if the current BaseAi is a Moose
 		/// </summary>
 		/// <param name="baseAi">The BaseAi instance</param>
 		/// <returns></returns>
-		public static bool IsMoose(BaseAi baseAi) => baseAi.Moose;
-
-        /// <summary>
-        /// Gets all Deer
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsBaseDeer(BaseAi baseAi) => baseAi.BaseDeer;
-        /// <summary>
-        /// Checks if the current BaseAi is a Normal Stag
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsStag(BaseAi baseAi) => baseAi.Stag;
-
-        /// <summary>
-        /// Checks if the current BaseAi is a White Stag
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsStagWhite(BaseAi baseAi) => baseAi.StagWhite;
-
-        /// <summary>
-        /// Checks if the current BaseAi is a doe
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsDoe(BaseAi baseAi) => baseAi.Doe;
-
-        /// <summary>
-        /// Checks if the current BaseAi is a ptarmigan
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsPtarmigan(BaseAi baseAi) => baseAi.Ptarmigan;
-
-        /// <summary>
-        /// Checks if the current BaseAi is a cougar
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsCougar(BaseAi baseAi) => baseAi.Cougar;
-
-        /// <summary>
-        /// Checks if the current BaseAi is valid
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns>true if the BaseAi is valid</returns>
-        /// <remarks>
-        /// <para>Checks if the current BaseAi mode is either <see cref="AiMode.None"/> or <see cref="AiMode.Disabled"/></para>
-        /// </remarks>
-        public static bool IsValid(BaseAi baseAi) => baseAi.m_CurrentMode != AiMode.None && baseAi.m_CurrentMode != AiMode.Disabled;
-
-        /// <summary>
-        /// Checks if the current BaseAi is dead
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns></returns>
-        public static bool IsDead(BaseAi baseAi) => baseAi.m_CurrentMode == AiMode.Dead;
-
-        /// <summary>
-        /// Gets the WildlifeMode for the given BaseAi
-        /// </summary>
-        /// <param name="baseAi">The BaseAi instance</param>
-        /// <returns>Current BaseAi instance WildlifeMode</returns>
-        /// <remarks>
-        /// <para>This is useful for when you want to know if its a normal animal or an Aurora animal</para>
-        /// </remarks>
-        public static WildlifeMode GetWildlifeMode(BaseAi baseAi) => baseAi.m_WildlifeMode;
-
+		public static bool IsMoose(BaseAi baseAi) => baseAi.Moose != null;
+		/// <summary>
+		/// Gets all wolves
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsBaseWolf(BaseAi baseAi) => baseAi.BaseWolf != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a normal wolf
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsNormalWolf(BaseAi baseAi) => baseAi.NormalWolf != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a timberwolf
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsTimberwolf(BaseAi baseAi) => baseAi.Timberwolf != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a Poisoned Wolf
+		/// </summary>
+		/// <param name="baseAi"></param>
+		/// <returns></returns>
+		public static bool IsToxicWolf(BaseAi baseAi) => baseAi.StarvingWolf != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a bear
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsBear(BaseAi baseAi) => baseAi.Bear != null;
+		/// <summary>
+		/// Checks if the current BaseAi is a cougar
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsCougar(BaseAi baseAi) => baseAi.Cougar != null;
+		/// <summary>
+		/// Checks if the current BaseAi is valid
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns><see langword="true"/> if the BaseAi is valid</returns>
+		/// <remarks>
+		/// <para>Checks if the current BaseAi mode is either <see cref="AiMode.None"/> or <see cref="AiMode.Disabled"/></para>
+		/// </remarks>
+		public static bool IsValid(BaseAi baseAi) => baseAi.m_CurrentMode != AiMode.None && baseAi.m_CurrentMode != AiMode.Disabled;
+		/// <summary>
+		/// Checks if the current BaseAi is dead
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns></returns>
+		public static bool IsDead(BaseAi baseAi) => baseAi.m_CurrentMode == AiMode.Dead;
+		/// <summary>
+		/// Gets the WildlifeMode for the given BaseAi
+		/// </summary>
+		/// <param name="baseAi">The BaseAi instance</param>
+		/// <returns>Current BaseAi instance WildlifeMode</returns>
+		/// <remarks>
+		/// <para>This is useful for when you want to know if its a normal animal or an Aurora animal</para>
+		/// </remarks>
+		public static WildlifeMode GetWildlifeMode(BaseAi baseAi) => baseAi.m_WildlifeMode;
 	}
 }
