0002905afc18831a373b4ebc0585e1f4087f6364
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 17:55:03 -0300

    Update SceneUtilities.cs

--- VisualStudio/Utilities/SceneUtilities.cs
+++ VisualStudio/Utilities/SceneUtilities.cs
@@ -8,8 +8,6 @@
 // Ensure you change the namespace to whatever namespace your mod uses, so it doesnt conflict with other mods
 // ---------------------------------------------
 
-using System.Text.Json.Serialization;
-
 namespace CommonLib.Utilities
 {
 	/// <summary>
@@ -18,23 +16,28 @@ namespace CommonLib.Utilities
 	public class SceneUtilities
 	{
 		/// <summary>
+		/// Gets the currently active scene from <see cref="UnityEngine.SceneManagement.SceneManager.GetActiveScene()"/>
+		/// </summary>
+		/// <returns>The name of the current scene</returns>
+		public static string GetActiveSceneName()
+			=> UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+
+		/// <summary>
 		/// Get if the current scene is indoor
 		/// </summary>
 		/// <param name="scene">true if you want the scene, false if you want the environment</param>
 		/// <returns></returns>
 		public static bool IsSceneIndoor(bool scene)
-		{
-			return (GameManager.GetWeatherComponent().IsIndoorScene() && scene) || GameManager.GetWeatherComponent().IsIndoorEnvironment();
-		}
+			=> (GameManager.GetWeatherComponent().IsIndoorScene() && scene) || GameManager.GetWeatherComponent().IsIndoorEnvironment();
 
 		/// <summary>
 		/// Used to check if the current scene is EMPTY
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneEmpty(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && sceneName.Contains("Empty", StringComparison.InvariantCultureIgnoreCase);
 		}
@@ -42,11 +45,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is BOOT
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneBoot(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && sceneName.Contains("Boot", StringComparison.InvariantCultureIgnoreCase);
 		}
@@ -54,11 +57,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is a main menu scene
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneMenu(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && sceneName.StartsWith("MainMenu", StringComparison.InvariantCultureIgnoreCase);
 		}
@@ -66,11 +69,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is a playable scene. Use this for most needs
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsScenePlayable(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && !IsSceneEmpty(sceneName) && !IsSceneBoot(sceneName) && !IsSceneMenu(sceneName);
 		}
@@ -78,11 +81,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is a base scene (Zone or Region)
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneBase(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && (sceneName.Contains("Region", StringComparison.InvariantCultureIgnoreCase) || sceneName.Contains("Zone", StringComparison.InvariantCultureIgnoreCase));
 		}
@@ -90,11 +93,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is a sandbox scene
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneSandbox(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && sceneName.EndsWith("SANDBOX", StringComparison.InvariantCultureIgnoreCase);
 		}
@@ -102,11 +105,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is a DLC01 scene
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneDLC01(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && sceneName.EndsWith("DLC01", StringComparison.InvariantCultureIgnoreCase);
 		}
@@ -114,11 +117,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is a DARKWALKER scene
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneDarkWalker(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && sceneName.Contains("DARKWALKER", StringComparison.InvariantCultureIgnoreCase);
 		}
@@ -126,11 +129,11 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is an additive scene, like sandbox or DLC scenes added to the base scene
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <returns></returns>
 		public static bool IsSceneAdditive(string? sceneName = null)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			return sceneName != null && (IsSceneSandbox(sceneName) || IsSceneDLC01(sceneName) || IsSceneDarkWalker(sceneName));
 		}
@@ -138,12 +141,12 @@ namespace CommonLib.Utilities
 		/// <summary>
 		/// Used to check if the current scene is valid for weather
 		/// </summary>
-		/// <param name="sceneName">The name of the scene to check, if null will use <c>GameManager.m_ActiveScene</c></param>
+		/// <param name="sceneName">The name of the scene to check, if null will use <see cref="GetActiveSceneName"/></param>
 		/// <param name="IndoorOverride"></param>
 		/// <returns></returns>
 		public static bool IsValidSceneForWeather(string sceneName, bool IndoorOverride)
 		{
-			sceneName ??= GameManager.m_ActiveScene;
+			sceneName ??= GetActiveSceneName();
 
 			// this is done this way to make it easier to see the logic
 			return sceneName != null
