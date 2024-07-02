a943c6e76b7d249bbbc465f4f2704fe79569c14e
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 17:56:59 -0300

    Update Extensions.cs

--- VisualStudio/Utilities/Extensions.cs
+++ VisualStudio/Utilities/Extensions.cs
@@ -7,50 +7,71 @@
 // Warning !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Ensure you change the namespace to whatever namespace your mod uses, so it doesnt conflict with other mods
 // ---------------------------------------------
+using Il2CppTLD.IntBackedUnit;
 
 namespace CommonLib.Utilities
 {
-    /// <summary>
-    /// Class containing all extensions
-    /// </summary>
-    public static class Extensions
-    {
-        /// <summary>
-        /// Gets every class that is derived from another
-        /// </summary>
-        /// <typeparam name="T">The typeof for the base class</typeparam>
-        /// <param name="constructorArgs">Any extra arguments</param>
-        /// <returns>A list of classes that are derived from <typeparamref name="T"/></returns>
-        public static System.Collections.Generic.IEnumerable<T>? GetEnumerableOfType<T>(params object[] constructorArgs) where T : class
-        {
-            if (typeof(T) == null) return null;
+	/// <summary>
+	/// Class containing all extensions
+	/// </summary>
+	public static class Extensions
+	{
+		/// <summary>
+		/// Gets every class that is derived from another
+		/// </summary>
+		/// <typeparam name="T">The typeof for the base class</typeparam>
+		/// <param name="constructorArgs">Any extra arguments</param>
+		/// <returns>A list of classes that are derived from <typeparamref name="T"/></returns>
+		public static System.Collections.Generic.IEnumerable<T>? GetEnumerableOfType<T>(params object[] constructorArgs) where T : class
+		{
+			if (typeof(T) == null) return null;
 
-            System.Collections.Generic.List<T?> objects = new();
+			System.Collections.Generic.List<T?> objects = new();
 
 #pragma warning disable CS8602
-            foreach (var type in System.Reflection.Assembly.GetAssembly(typeof(T)).GetTypes().Where(
-                t => t.IsClass
-                && !t.IsAbstract
-                && t.IsSubclassOf(typeof(T))
-                ))
-            {
-                objects.Add(System.Activator.CreateInstance(type, constructorArgs) as T);
-            }
+			foreach (var type in System.Reflection.Assembly.GetAssembly(typeof(T)).GetTypes().Where(
+				t => t.IsClass
+				&& !t.IsAbstract
+				&& t.IsSubclassOf(typeof(T))
+				))
+			{
+				objects.Add(System.Activator.CreateInstance(type, constructorArgs) as T);
+			}
 #pragma warning restore CS8602
 
-            try
-            {
-                objects?.Sort();
-            }
-            catch (System.InvalidOperationException e)
-            {
-                Main.Logger.Log($"GetEnumerableOfType::Attempting to sort the objects list failed", FlaggedLoggingLevel.Exception, e);
-            }
+			try
+			{
+				objects?.Sort();
+			}
+			catch (System.InvalidOperationException e)
+			{
+				Main.Logger.Log($"GetEnumerableOfType::Attempting to sort the objects list failed", FlaggedLoggingLevel.Exception, e);
+			}
 
 #pragma warning disable CS8619
-            // returns null if the list is null or has nothing in it
-            return (objects == null) ? null : (objects.Count > 0) ? objects : null;
+			// returns null if the list is null or has nothing in it
+			return (objects == null) ? null : (objects.Count > 0) ? objects : null;
 #pragma warning restore CS8619
-        }
-    }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="itemWeight"></param>
+		/// <returns></returns>
+		public static double GetValueFromItemWeight(this ItemWeight itemWeight)
+		{
+			return itemWeight.ToQuantity(itemWeight.m_Units);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="volume"></param>
+		/// <returns></returns>
+		public static double GetLiquidLitersFromItemLiquidVolume(this ItemLiquidVolume volume)
+		{
+			return volume.ToQuantity(ItemLiquidVolume.Liter.m_Units);
+		}
+	}
 }
