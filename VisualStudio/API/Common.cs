05c8e1955bc8fcf4be93e4a8fd86d9cdb042b7c5
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 17:50:05 -0300

    Create Common.cs

---
+++ VisualStudio/API/Common.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CommonLib.API
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public sealed class Common
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		[JsonInclude]
+		public static List<object> RegisteredGlobals = new();
+	}
+}
