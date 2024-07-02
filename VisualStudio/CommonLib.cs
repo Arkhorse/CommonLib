cc9a99158f92be98c0408d5ee872a1582f846bb6
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 17:48:09 -0300

    Update CommonLib.cs

--- VisualStudio/CommonLib.cs
+++ VisualStudio/CommonLib.cs
@@ -8,13 +8,14 @@ global using UnityEngine.UIElements;
 global using UnityEngine.AddressableAssets;
 #endregion
 #region Mod Directives
-global using CommonLib.API;
 global using CommonLib.Utilities;
 global using CommonLib.Utilities.Enums;
 global using CommonLib.Utilities.Exceptions;
 global using CommonLib.Utilities.JSON;
 global using ComplexLogger;
 #endregion
+
+using System.Text.Json.Serialization;
 
 namespace CommonLib
 {
@@ -32,11 +33,6 @@ namespace CommonLib
 		/// </summary>
 		[JsonInclude]
 		public static List<object> RegisteredGlobals = new();
-
-		/// <summary>
-		/// 
-		/// </summary>
-		public static Globals Globals = new();
 
 		/// <inheritdoc/>
 		public override void OnInitializeMelon()
