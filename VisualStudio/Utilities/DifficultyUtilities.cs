2e4a8390d0ae2dbf81d06e5649f3326927647b89
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 17:57:10 -0300

    Update DifficultyUtilities.cs

--- VisualStudio/Utilities/DifficultyUtilities.cs
+++ VisualStudio/Utilities/DifficultyUtilities.cs
@@ -6,51 +6,56 @@ namespace CommonLib.Utilities
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 	{
 		/// <summary>
-		/// 
+		/// Gets the current difficulty mode
 		/// </summary>
-		public static Dictionary<Mode, Levels> DifficultyLibrary { get; } = new()
-		{
-			{ Mode.StoryMode, Levels.None },
-			{ Mode.Sandbox, Levels.Pilgrim }
-		};
-
-		/// <summary>
-		/// Gets the current difficulty mode, each with its own int
-		/// </summary>
-		/// <returns>
-		/// </returns>
-		public static int GetCurrentMode()
+		/// <returns></returns>
+		public static Difficulty GetCurrentMode()
 		{
 			return ExperienceModeManager.GetCurrentExperienceModeType() switch
 			{
 				// Sandbox 1-9
-				ExperienceModeType.Pilgrim					=> 1,
-				ExperienceModeType.Voyageur					=> 2,
-				ExperienceModeType.Stalker					=> 3,
-				ExperienceModeType.Interloper				=> 4,
+				ExperienceModeType.Pilgrim					=> Difficulty.Pilgrim,
+				ExperienceModeType.Voyageur					=> Difficulty.Voyageur,
+				ExperienceModeType.Stalker					=> Difficulty.Stalker,
+				ExperienceModeType.Interloper				=> Difficulty.Interloper,
 
-				ExperienceModeType.Custom					=> 0,
+				ExperienceModeType.Custom					=> Difficulty.Custom,
 
 				// Wintermute 10-19
-				ExperienceModeType.Story					=> 10,
-				ExperienceModeType.StoryFresh				=> 11,
-				ExperienceModeType.StoryHardened			=> 12,
+				ExperienceModeType.Story					=> Difficulty.Story,
+				ExperienceModeType.StoryFresh				=> Difficulty.Story,
+				ExperienceModeType.StoryHardened			=> Difficulty.Story,
 
 				// Challenges 20-39
-				ExperienceModeType.ChallengeRescue			=> 20,
-				ExperienceModeType.ChallengeWhiteout		=> 21,
-				ExperienceModeType.ChallengeNomad			=> 22,
-				ExperienceModeType.ChallengeHunted			=> 23,
-				ExperienceModeType.ChallengeHuntedPart2		=> 24,
-				ExperienceModeType.ChallengeArchivist		=> 25,
-				ExperienceModeType.ChallengeDeadManWalking	=> 26,
-				ExperienceModeType.ChallengeNowhereToHide	=> 27,
+				ExperienceModeType.ChallengeRescue			=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeWhiteout		=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeNomad			=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeHunted			=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeHuntedPart2		=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeArchivist		=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeDeadManWalking	=> Difficulty.Challenge,
+				ExperienceModeType.ChallengeNowhereToHide	=> Difficulty.Challenge,
 
 				// Events 40+
-				ExperienceModeType.EventWintersEmbrace		=> 40,
-				ExperienceModeType.FourDaysOfNight			=> 41,
-				_											=> -1,
+				ExperienceModeType.EventWintersEmbrace		=> Difficulty.Event,
+				ExperienceModeType.FourDaysOfNight			=> Difficulty.Event,
+				_											=> Difficulty.None,
 			};
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public enum Difficulty
+		{
+			None,
+			Pilgrim,
+			Voyageur,
+			Stalker,
+			Interloper,
+			Custom,
+			Challenge,
+			Story,
+			Event
 		}
 	}
 }
